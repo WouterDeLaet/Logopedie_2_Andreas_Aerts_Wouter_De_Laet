a63ea856285003b5bb82483d9ae5b3603b37aa00
WouterDeLaet <wouter@sommico.com>
2018-12-11 11:39:35 +0100

    Navigation

--- app/src/main/java/be/thomasmore/logopedie_2_andreas_aerst_wouter_de_laet/writing_test.java
+++ app/src/main/java/be/thomasmore/logopedie_2_andreas_aerst_wouter_de_laet/writing_test.java
@@ -1,33 +1,32 @@
 package be.thomasmore.logopedie_2_andreas_aerst_wouter_de_laet;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
-public class StartScherm extends AppCompatActivity {
+public class writing_test extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_scherm);
+        setContentView(R.layout.activity_writing_test);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     public void goToSpeechToText_onClick(MenuItem m)
@@ -46,13 +45,5 @@ public class StartScherm extends AppCompatActivity {
     {
         Intent intent = new Intent(this, StartScherm.class);
         startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu_main, menu);
-        return true;
     }
 }
